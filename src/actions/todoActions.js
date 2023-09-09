6eb9c38abee35097aae3753d0bdc33eaffe76e7f
Jagadish Vundapalli <jagadishvundapalli@VJPC0110042022.local>
2023-09-09 15:55:10 +0530

    react-redux sample

---
+++ src/actions/todoActions.js
@@ -0,0 +1,18 @@
+import { ADD_TODO, TOGGLE_TODO, DELETE_TODO } from './types';
+
+// Action creators
+export const addTodo = (text) => ({
+  type: ADD_TODO,
+  payload: text,
+});
+
+export const toggleTodo = (id) => ({
+  type: TOGGLE_TODO,
+  payload: id,
+});
+
+export const deleteTodo = (id) => ({
+  type: DELETE_TODO,
+  payload: id,
+});
+
---
+++ src/reducers/todoReducer.js
@@ -0,0 +1,43 @@
+import { ADD_TODO, TOGGLE_TODO, DELETE_TODO } from "../actions/types";
+
+const initialState = {
+  todos: [
+    { text: "task1", id: 1 },
+    { text: "task2", id: 2 },
+  ],
+};
+
+const todoReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return {
+        ...state,
+        todos: [
+          ...state.todos,
+          {
+            id: Date.now(),
+            text: action.payload,
+            completed: false,
+          },
+        ],
+      };
+    case TOGGLE_TODO:
+      return {
+        ...state,
+        todos: state.todos.map((todo) =>
+          todo.id === action.payload
+            ? { ...todo, completed: !todo.completed }
+            : todo
+        ),
+      };
+    case DELETE_TODO:
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo.id !== action.payload),
+      };
+    default:
+      return state;
+  }
+};
+
+export default todoReducer;
